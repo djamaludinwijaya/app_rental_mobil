1bfc69435828e81d59732448a0d700f96c018870
djamaludinwijaya <djamaludin.wijaya@gmail.com>
2021-02-03 16:03:03 +0700

    melengkapi form tambah supir

--- application/views/admin/form_tambah_supir.php
+++ application/views/admin/form_tambah_supir.php
@@ -35,13 +35,35 @@
                             <?= form_error('alamat_supir', '<span class="text-small text-danger">', '</span>'); ?>
                         </div>
 
-
-
                         <div class="form-group">
                             <label for="no_telepon">No Telepon</label>
                             <input type="text" name="no_telepon" class="form-control" id="no_telepon">
                             <?= form_error('no_telepon', '<span class="text-small text-danger">', '</span>'); ?>
                         </div>
+
+                        <div class="form-group">
+                            <label for="status_supir">Status Supir</label>
+                            <select name="status_supir" id="status_supir" class="form-control">
+                                <option value="">Pilih Status Supir</option>
+                                <option value="0">Tersedia</option>
+                                <option value="1">Tidak Tersedia</option>
+                            </select>
+                            <?= form_error('status_supir', '<span class="text-small text-danger">', '</span>'); ?>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="tarif_dalam_kota">Tarif Supir Dalam Kota</label>
+                            <input type="number" name="tarif_dalam_kota" class="form-control" id="tarif_dalam_kota">
+                            <?= form_error('tarif_dalam_kota', '<span class="text-small text-danger">', '</span>'); ?>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="tarif_luar_kota">Tarif Supir Luar Kota</label>
+                            <input type="number" name="tarif_luar_kota" class="form-control" id="tarif_luar_kota">
+                            <?= form_error('tarif_luar_kota', '<span class="text-small text-danger">', '</span>'); ?>
+                        </div>
+
+
                         <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
                         <button type="reset" class="btn btn-danger btn-sm">Reset</button>
                     </form>
