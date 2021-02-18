3ee782bbc0c1bb002db7705822ca08258a903974
djamaludinwijaya <djamaludin.wijaya@gmail.com>
2021-02-18 15:19:07 +0700

    memperbaiki halaman dashboard

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,9 +3,19 @@ class Dashboard extends CI_Controller
 {
     public function index()
     {
+        $mobil = $this->db->get('mobil');
+        $type = $this->db->get('type');
+        $supir = $this->db->get('supir');
+        $customer = $this->db->get('customer');
+
+        $data['mobil'] = $mobil->num_rows();
+        $data['type'] = $type->num_rows();
+        $data['supir'] = $supir->num_rows();
+        $data['customer'] = $customer->num_rows();
+
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/sidebar');
-        $this->load->view('admin/dashboard');
+        $this->load->view('admin/dashboard', $data);
         $this->load->view('templates_admin/footer');
     }
 }
